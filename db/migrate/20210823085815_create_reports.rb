9b82c49671979fc4d908be030d0d8deff540daf7
IT0825 <ace0829.1114@icloud.com>
2021-08-23 18:02:06 +0900

    reportカラム作成

--- db/migrate/20210823085815_create_reports.rb
+++ db/migrate/20210823085815_create_reports.rb
@@ -1,7 +1,8 @@
 class CreateReports < ActiveRecord::Migration[6.0]
   def change
     create_table :reports do |t|
-
+      t.string      :title,  null: false, limit: 30
+      t.text        :text,   null: false
       t.timestamps
     end
   end
