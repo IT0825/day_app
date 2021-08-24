5d271f721c47ee6e4a602925d3afbfccef5fdb42
IT0825 <ace0829.1114@icloud.com>
2021-08-24 14:24:07 +0900

    textのバリデーションに５文字以上のバリデーション設定

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,6 +1,6 @@
 class Report < ApplicationRecord
 
   validates :title, presence: true, length: { maximum: 30 }
-  validates :text,  presence: true
+  validates :text,  presence: true, length: { minimum: 5 }
   
 end
