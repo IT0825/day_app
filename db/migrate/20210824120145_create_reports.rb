e5b1dd6283fc3c5ee1d4fcc20484835157536022
IT0825 <ace0829.1114@icloud.com>
2021-08-24 21:09:12 +0900

    deviseモデル作成

--- db/migrate/20210824120145_create_reports.rb
+++ db/migrate/20210824120145_create_reports.rb
@@ -3,6 +3,7 @@ class CreateReports < ActiveRecord::Migration[6.0]
     create_table :reports do |t|
       t.string      :title,  null: false, limit: 30
       t.text        :text,   null: false
+      t.references :user,   null: false, foreign_key:true
       t.timestamps
     end
   end
