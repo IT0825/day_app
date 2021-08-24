8d430f6e54b29cc68a5fe87e19f20fb0c6c3d733
IT0825 <ace0829.1114@icloud.com>
2021-08-24 14:52:00 +0900

    Flashメッセージの実装

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -11,7 +11,7 @@ class ReportsController < ApplicationController
     @report = Report.new(report_params)
     if @report.valid?
       @report.save
-      redirect_to root_path
+      redirect_to root_path, notice: "日報「#{@report.title}」を作成しました。"
     else
       render :new
     end
