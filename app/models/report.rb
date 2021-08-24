0751e5c52b9941e5097343b305596c19c5cb2a49
IT0825 <ace0829.1114@icloud.com>
2021-08-24 21:14:28 +0900

    userとreportのバリデーションとアソシエーションの設定

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,5 +1,7 @@
 class Report < ApplicationRecord
 
+  belongs_to :user
+
   validates :title, presence: true, length: { maximum: 30 }
   validates :text,  presence: true, length: { minimum: 5 }
   
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :reports
+
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+  validates :name, presence: true
+  validates :password, presence: true, format: { with: VALID_PASSWORD_REGEX }
 end
