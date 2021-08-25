fca5bf30e11aa64d5e89e6c13f9a8306c3f167b1
IT0825 <ace0829.1114@icloud.com>
2021-08-26 06:30:23 +0900

    ユーザー削除機能実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,6 +20,12 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    user = User.find(params[:id])
+    user.destroy
+    redirect_to users_path, notice: "ユーザー「#{user.name}」さんを削除しました。"
+  end
+
   private
 
   def user_params
