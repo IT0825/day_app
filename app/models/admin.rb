790f63d955549138837189ba959734c17ed51955
IT0825 <ace0829.1114@icloud.com>
2021-08-25 13:57:27 +0900

    adminモデルにバリデーション設定

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -3,4 +3,9 @@ class Admin < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+
+  validates :name, presence: true
+  validates :password, presence: true, format: { with: VALID_PASSWORD_REGEX }
 end
