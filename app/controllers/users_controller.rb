2c9358d7910640d16f2242d1b214e7fab1b2cd4d
IT0825 <ace0829.1114@icloud.com>
2021-08-26 07:32:51 +0900

    ユーザー編集機能は管理者のみ遷移できるように設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update]
-  before_action :move_to_index, only: [:show, :edit, :update]
+  before_action :move_to_index, only: :show
+  before_action :metastasis_show, only: [:index, :edit, :update]
 
   def index
     @users = User.all
@@ -42,6 +43,12 @@ class UsersController < ApplicationController
     elsif admin_signed_in?
     else
       redirect_to root_path
+    end
+  end
+
+  def metastasis_show
+    unless admin_signed_in?
+      redirect_to action: :show
     end
   end
 end
