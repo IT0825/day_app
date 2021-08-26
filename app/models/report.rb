88c44b6ecbdd7cfebc3a792533bff03ebd81cda9
IT0825 <ace0829.1114@icloud.com>
2021-08-26 15:15:48 +0900

    作成日時での検索フォーム作成

--- app/models/report.rb
+++ app/models/report.rb
@@ -4,5 +4,13 @@ class Report < ApplicationRecord
 
   validates :title, presence: true, length: { maximum: 30 }
   validates :text,  presence: true, length: { minimum: 5 }
+
+  def self.ransackable_attributes(auth_object = nil)
+    %w[title created_at]
+  end
+
+  def self.ransackable_associations(auth_object = nil)
+    []
+  end
   
 end
